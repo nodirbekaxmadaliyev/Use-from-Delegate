c121fa44bea38874c0fe785d1f49a50e06bdf904
user <user@WIN-6V4LPHM0H9P>
2024-11-14 14:55:47 +0500

    Add project files.

---
+++ Delegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Delegate
+{
+    internal class Program
+    {
+        public delegate void math(int a, int b);
+        public void sum(int a, int b)
+        {
+            Console.WriteLine( a + b);
+        }
+        public void sub(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+
+        static void Main(string[] args)
+        {
+            Program ob = new Program();
+            math Dsum = new math(ob.sum);
+            Dsum += ob.sub;
+            Dsum(25, 15);
+            Dsum.Invoke(25, 35);
+        }
+
+    }
+}
